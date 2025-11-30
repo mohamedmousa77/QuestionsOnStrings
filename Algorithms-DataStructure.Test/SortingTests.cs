f714ce6c473845f2b27bd4e8e55d06f6e874171b
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2025-11-30 05:52:50 +0100

    Sorting Algorithms with CSharp

---
+++ Algorithms-DataStructure.Test/SortingTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using QuestionsOnStrings;
+
+namespace Algorithms_DataStructure.Test
+{
+    [TestFixture]
+    public class SortingTests
+    {
+        private int[][] Samples()
+        {
+            int[][] samples =
+            [
+                [1],
+                [2, 1],
+                [2, 1, 3],
+                [1, 1, 1],
+                [2, -1, 3, 3],
+                [4, -5, 3, 3],
+                [0, -5, 3, 3],
+                [0, -5, 3, 0],
+                [3, 2, 5, 5, 1, 0, 7, 8],
+            ];
+            return samples;
+        }
+
+        public void RunTestsForSortAlgorithm(Action<int[]> sort )
+        {
+            foreach (int[] sample in Samples()) 
+            {
+                sort(sample);
+                CollectionAssert.IsOrdered(sample);
+                PrintOut(sample);
+            }
+        }
+
+        private void PrintOut(int[] array)
+        {
+            TestContext.WriteLine("----------TRACE---------\n");
+            foreach(int el in array)
+            {
+                TestContext.Write(el + " ");
+            }
+            TestContext.WriteLine("\n--------------\n");
+        }
+
+        [Test]
+        public void ShellSort_ValidInput_SortedOutput()
+        {
+            RunTestsForSortAlgorithm(Sorting_Algorithms.ShellSort);
+        }
+
+        [Test]
+        public void SelectionSort_ValidInput_SortedOutput()
+        {
+            RunTestsForSortAlgorithm(Sorting_Algorithms.SelectionSort);
+        }
+
+        [Test]
+        public void BubbleSort_ValidInput_SortedOutput()
+        {
+            RunTestsForSortAlgorithm(Sorting_Algorithms.BubbleSort);
+        }
+
+    }
+}
---
+++ QuestionsOnStrings/Sorting Algorithms.cs
@@ -0,0 +1,62 @@
+﻿
+namespace QuestionsOnStrings
+{
+    public class Sorting_Algorithms
+    {
+
+        public static void ShellSort(int[] array)
+        {
+            for (int partIndex = 1; partIndex < array.Length; partIndex++)
+            {
+                int currentUnsorted = array[partIndex];
+                int i = 0;
+                for(i=partIndex; i>0 && array[i-1] > currentUnsorted; i--)
+                {
+                    array[i] = array[i-1];
+                }
+                array[i] = currentUnsorted;
+            }
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+           
+            for(int partIndex = array.Length-1; partIndex > 0; partIndex--)
+            {
+                int largestAt = 0;
+                for (int i = 1; i <= partIndex; i++)
+                {
+                    if(array[i] > array[largestAt])
+                    {
+                        largestAt = i;
+                    }
+                }
+                Swap(array,largestAt, partIndex);
+            }
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for(int partIndex = array.Length - 1; partIndex > 0; partIndex--)
+            {
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if (array[i] >  array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            int temp;
+
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
