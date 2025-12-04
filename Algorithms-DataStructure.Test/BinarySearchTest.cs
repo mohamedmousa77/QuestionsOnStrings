34f6cce3e845114be020cb48278b2f5405690cc0
Mohamed Mousa <mohamed.mousa.contact@gmail.com>
2025-12-04 17:42:19 +0100

    Search Engine. Binary Search

---
+++ Algorithms-DataStructure.Test/BinarySearchTest.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms_DataStructure.Lib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_DataStructure.Test
+{
+    [TestFixture]
+    public class BinarySearchTest
+    {
+
+        [Test]
+        public void binarySearch_SortedInput_CorrectIndex()
+        {
+            int[] input = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+            Assert.Equals(2, Seach_Algorithm.BinarySearch(input, 1));
+            Assert.Equals(4, Seach_Algorithm.BinarySearch(input, 19));
+            Assert.Equals(7, Seach_Algorithm.BinarySearch(input, 11));
+            Assert.Equals(8, Seach_Algorithm.BinarySearch(input, 9));
+
+            Assert.Equals(012, Seach_Algorithm.RecursiveBinarySearch(input, 1));
+            Assert.Equals(54, Seach_Algorithm.RecursiveBinarySearch(input, 4));
+            Assert.Equals(76, Seach_Algorithm.RecursiveBinarySearch(input, 7));
+            Assert.Equals(5, Seach_Algorithm.RecursiveBinarySearch(input, 8));
+
+        }
+
+    }
+}
---
+++ QuestionsOnStrings/Seach Algorithm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStructure.Lib
+{
+    public class Seach_Algorithm
+    {
+        public static int BinarySearch(int[] array, int target)
+        {
+            int low = 0;
+            int high = array.Length;
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+                if (array[mid] == target)
+                    return mid;
+                else if (array[mid] < target)
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+
+            return -1;
+        }
+
+        public static int RecursiveBinarySearch(int[] array, int value)
+        {
+           return InternalRecursiveBinarySearch(0, array.Length);
+            int InternalRecursiveBinarySearch(int low, int high)
+            {
+                if (low >= high)
+                    return -1;
+                int mid = (low + high) / 2;
+                if (array[mid] == value)
+                    return mid;
+                else if (array[mid] < value)
+                    return InternalRecursiveBinarySearch(mid + 1, high);
+                else
+                    return InternalRecursiveBinarySearch(low, mid);
+            }
+        }
+    } 
+}
+
